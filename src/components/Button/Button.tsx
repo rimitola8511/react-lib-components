11555e59f0000d2dd7d6a649b7ac6d6bc8de4526
RicardoImitola <ricardo@wareclouds.com>
2023-09-06 18:16:51 -0500

    ✨react library

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode | ReactNode[];
+}
+
+const Button: FC<Props> = ({ children }) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
